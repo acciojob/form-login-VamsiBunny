1c4098ab149bef5fa4e836ab03d76c8332d8cd76
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-14 21:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
+let fname=document.getElementById("fname");
+let laname=document.getElementById("lname");
+let btn=document.getElementById("btn");
 
-}
+btn.addEventListener("click",()=>{
+	alert(`${fname.value}+${laname.value}`)
+})
